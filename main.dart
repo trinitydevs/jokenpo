f7d5e536f6fe266dc64e92cdb72236b5f26c8420
Trinity <111662820+TrinityDN@users.noreply.github.com>
2023-05-18 10:49:34 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'jogo.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    //Importando
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+    ));
+ 
+}
